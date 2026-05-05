8f6769ffcd1aa31d5e2823204b2c647eda78fbdb
Koppula Varun <varunkoppula2007@gmail.com>
2026-05-05 11:16:05 +0530

    Refactor Room model schema for improved readability and add JWT flow documentation

--- backend/models/Room.js
+++ backend/models/Room.js
@@ -1,29 +1,32 @@
-import {Schema , model } from 'mongoose';
+import { Schema, model } from 'mongoose';
 
 const roomSchema = new Schema({
-    roomId:{
-        type:String,
-        required:[true,"Please provide a room id"],
-        unique:true,
-        trim:true
-    },
-    code:{
-        type :String,
-        default:""
-        
-    },
-    language:{
-        type:String,
-        enum:["javascript","python","java","c++","c","ruby","go","php"],
-        default:"javascript"
-    },
-
-},{
-    versionKey:false,
-    timestamps:true,
-    strict:"throw"
+  roomId: {
+    type: String,
+    required: [true, "Please provide a room id"],
+    unique: true,
+    trim: true
+  },
+  code: {
+    type: String,
+    default: ""
+  },
+  language: {
+    type: String,
+    enum: ["javascript", "python", "java", "c++", "c", "ruby", "go", "php"],
+    default: "javascript"
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  }
+}, {
+  versionKey: false,
+  timestamps: true,
+  strict: "throw"
 });
 
-const Room = model('Room',roomSchema);
+const Room = model('Room', roomSchema);
 
 export default Room;
---
+++ flows.md
@@ -0,0 +1,23 @@
+### jwt flow 
+🔹 Login
+1. user logs in
+2. backend verifies credentials
+3. generate JWT → { userId }
+4. send token
+🔹 Protected route
+1. frontend sends token
+2. backend verifies token
+3. extract userId
+4. fetch user from DB
+
+note : we store jwt {userid} because 
+            ❌Why NOT email?
+            Email can change
+            Slightly bigger payload
+            Not needed for DB queries
+            ❌ Why NOT provider?
+            Not required for authentication
+            Adds unnecessary complexity
+            You can always fetch it from DB
+
+
