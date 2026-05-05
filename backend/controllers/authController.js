338c2272884c252847e6fd8176004f33271690c5
Nimishakavi Sri Nihal <nimishakavisrinihal@gmail.com>
2026-05-05 14:18:53 +0530

    Implement login functionality with JWT authentication and password validation

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -32,3 +32,31 @@ export const register = async (req, res) => {
 
 };
 
+
+export const login = async(req,res)=>{
+
+  try{
+  const{email , password} = req.body
+
+  if (!email || !password) {
+     return res.status(400).json({ error: 'All fields are required' });
+   }
+   const hashedPassword1 = await bcrypt.hash(password, 10);
+
+   const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+    //send cookie
+    res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: 'lax' });
+    res.status(200).json({ message: 'Login successful', payload: { username: user.username, id: user._id, email: user.email } });
+} catch (error) {
+    console.error('Error logging in user:', error);
+    res.status(500).json({ error: 'Server error' });
+  }
+}
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { register } from "../controllers/authController.js";
-
+import { login } from "../controllers/authController.js";
 const router = express.Router();
 
 // REGISTER
 router.post("/register", register);
+// LOGIN
+router.post("/login", login);
 
 export default router;
