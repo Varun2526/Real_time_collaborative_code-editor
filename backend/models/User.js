ea220dd3160d88d055761ddf36f445ed003df9ed
Kaza Venkata Hareesh <hareesh072022@gmail.com>
2026-05-05 10:37:07 +0530

    Add User model schema with required fields and default values

---
+++ backend/models/User.js
@@ -0,0 +1,20 @@
+import {Schema,model} from 'mongoose';
+const userSchema = new Schema({
+    username:{
+        type:String,
+        required:true
+    },
+   socketid:{
+        type:String,
+        required:true
+    },
+    currentRoom:{
+        type:String,
+        default:''
+    }
+},{
+    timestamps:true
+});
+
+export const User = model('User',userSchema);
+
