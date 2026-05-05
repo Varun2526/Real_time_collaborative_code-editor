733e59e2d03343cef007ef64ba514bb639fc78b2
Koppula Varun <varunkoppula2007@gmail.com>
2026-05-05 09:50:01 +0530

    Refactor server setup and add database connection logic

---
+++ backend/app.js
@@ -0,0 +1,59 @@
+import cookieParser from "cookie-parser";
+import cors from 'cors'
+import exp from "express";
+
+
+const app =exp();
+//unable cors 
+app.use(cors({
+  origin:["http://localhost:5174"
+],
+  credentials:true
+}))
+
+// Body parser
+app.use(exp.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+//routess wrire all the routes here 
+//app.use()
+
+//to handle invalid path
+app.use((req, res, next) => {
+  console.log(req.url);
+  res.status(404).json({ message: `path ${req.url} is invalid` });
+});
+
+
+//error handling middleware
+app.use((err, req, res, next) => {
+  console.log("error is ",err)
+  console.log("Full error:", JSON.stringify(err, null, 2));
+  //ValidationError
+  if (err.name === "ValidationError") {
+    return res.status(400).json({ message: "error occurred", error: err.message });
+  }
+  //CastError
+  if (err.name === "CastError") {
+    return res.status(400).json({ message: "error occurred", error: err.message });
+  }
+  const errCode = err.code ?? err.cause?.code ?? err.errorResponse?.code;
+  const keyValue = err.keyValue ?? err.cause?.keyValue ?? err.errorResponse?.keyValue;
+
+  if (errCode === 11000) {
+    const field = Object.keys(keyValue)[0];
+    const value = keyValue[field];
+    return res.status(409).json({
+      message: "error occurred",
+      error: `${field} "${value}" already exists`,
+    });
+  }
+
+  //send server side error
+  res.status(500).json({ message: "error occurred", error: "Server side error" });
+});
+
+// Export app for Vercel serverless
+export default app;
