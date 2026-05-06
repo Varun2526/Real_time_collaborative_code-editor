32382f9c310df3714db95efa58f3ba7e9d2f0353
Kaza Venkata Hareesh <hareesh072022@gmail.com>
2026-05-06 14:09:48 +0530

    Enhance message retrieval by validating room ID and updating chat route in request documentation

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -1,8 +1,8 @@
 import {MessageModel} from "../models/Message.js";
-
+import mongoose from "mongoose";
 export const getMessages=async(req,res)=>{
     try{
-        //get room id
+        //get object id of the room from the request parameters
         const {roomId}=req.params;
         //check whether the room id exists or not 
         if(!roomId){
@@ -10,10 +10,16 @@ export const getMessages=async(req,res)=>{
                 message:"Room id is required"
             });
         }
+        //validate Objectid
+        if(!mongoose.Types.ObjectId.isValid(roomId)){
+            return res.status(400).json({
+                message:"Invalid room id"
+            });
+        }
         //fetch messages based on the room id and sort them in ascending order based on createdAt
         const messages=await MessageModel.find({roomId}).populate("sender").sort({createdAt:1});
         //if no messages found return empty array
-        if(!messages){
+        if(!messages || messages.length === 0){
             return res.status(200).json({
                 message:"No messages found",
                 payload:[]
