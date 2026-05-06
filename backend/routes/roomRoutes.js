ae9082fa0ce89303c4109f5dc43d1116547deb6e
Koppula Varun <varunkoppula2007@gmail.com>
2026-05-06 10:34:44 +0530

    Rename router variable to roomRouter for clarity and consistency in room routes

--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -2,10 +2,10 @@ import express from "express";
 import { createRoom } from "../controllers/roomController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 import { getRoomById } from "../controllers/roomController.js";
-const router = express.Router();
+const roomRouter = express.Router();
 
 // CREATE ROOM
-router.post("/create", verifyToken, createRoom);
+roomRouter.post("/create", verifyToken, createRoom);
 //Get room by id
-router.get("/:roomId", verifyToken, getRoomById);
-export default router;
+roomRouter.get("/:roomId", verifyToken, getRoomById);
+export default roomRouter;
