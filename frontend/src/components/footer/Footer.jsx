f750fcfca35a8dedc4bef084cdb2c4b0292ea494
Koppula Varun <varunkoppula2007@gmail.com>
2026-05-11 15:24:16 +0530

    refactor: simplify footer links and update hover color states to blue

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import TermsModal from './TermsModal';
 
 const Footer = () => {
   return (
@@ -12,13 +11,8 @@ const Footer = () => {
         
         <div className="flex flex-col md:flex-row items-center gap-6 md:gap-8">
           <div className="flex flex-wrap justify-center items-center gap-6">
-            <Link to="/privacy-policy" className="text-spacex-nav text-white/40 hover:text-white transition-colors">PRIVACY POLICY</Link>
-            <button 
-              onClick={() => setIsTermsOpen(true)}
-              className="text-spacex-nav text-white/40 hover:text-white transition-colors cursor-pointer"
-            >
-              TERMS OF SERVICE
-            </button>
+            <Link to="/privacy-policy" className="text-spacex-nav text-white/40 hover:!text-[#0077b5] transition-colors">PRIVACY POLICY</Link>
+            <Link to="/terms-of-service" className="text-spacex-nav text-white/40 hover:!text-[#0077b5] transition-colors">TERMS OF SERVICE</Link>
             
             <a href="https://github.com/Varun2526/Real_time_collaborative_code-editor" target="_blank" rel="noreferrer" className="text-spacex-nav text-[#0077b5]/80 hover:text-[#0077b5] transition-colors">GITHUB</a>
             
@@ -29,11 +23,11 @@ const Footer = () => {
           
           <div className="flex flex-wrap justify-center items-center gap-4">
             <span className="text-spacex-micro text-white/70 hidden sm:inline mr-2">TEAM:</span>
-            <a href="https://www.linkedin.com/in/varun-koppula-b01916315/" target="_blank" rel="noreferrer" className="text-spacex-nav text-white/40 hover:text-white transition-colors uppercase">VARUN</a>
-            <a href="https://www.linkedin.com/in/venkata-hareesh-kaza-72b237330/" target="_blank" rel="noreferrer" className="text-spacex-nav text-white/40 hover:text-white transition-colors uppercase">HAREESH</a>
-            <a href="https://www.linkedin.com/in/nimishakavi-sri-nihal-723069271/" target="_blank" rel="noreferrer" className="text-spacex-nav text-white/40 hover:text-white transition-colors uppercase">NIHAL</a>
-            <a href="https://www.linkedin.com/in/bollam-jayaram-aditya/" target="_blank" rel="noreferrer" className="text-spacex-nav text-white/40 hover:text-white transition-colors uppercase">JAYARAM</a>
-            <a href="https://www.linkedin.com/in/nigama-vydyula-8554a83b6/" target="_blank" rel="noreferrer" className="text-spacex-nav text-white/40 hover:text-white transition-colors uppercase">NIGAMA</a>
+            <a href="https://www.linkedin.com/in/varun-koppula-b01916315/" target="_blank" rel="noreferrer" className="text-spacex-nav text-white/40 hover:!text-[#0077b5] transition-colors uppercase">VARUN</a>
+            <a href="https://www.linkedin.com/in/venkata-hareesh-kaza-72b237330/" target="_blank" rel="noreferrer" className="text-spacex-nav text-white/40 hover:!text-[#0077b5] transition-colors uppercase">HAREESH</a>
+            <a href="https://www.linkedin.com/in/nimishakavi-sri-nihal-723069271/" target="_blank" rel="noreferrer" className="text-spacex-nav text-white/40 hover:!text-[#0077b5] transition-colors uppercase">NIHAL</a>
+            <a href="https://www.linkedin.com/in/bollam-jayaram-aditya/" target="_blank" rel="noreferrer" className="text-spacex-nav text-white/40 hover:!text-[#0077b5] transition-colors uppercase">JAYARAM</a>
+            <a href="https://www.linkedin.com/in/nigama-vydyula-8554a83b6/" target="_blank" rel="noreferrer" className="text-spacex-nav text-white/40 hover:!text-[#0077b5] transition-colors uppercase">NIGAMA</a>
           </div>
         </div>
       </div>
