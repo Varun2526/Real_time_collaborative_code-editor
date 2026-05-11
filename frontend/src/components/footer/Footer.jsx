f6055717fe100865d8e15930d9b55ae2d7488e26
Koppula Varun <varunkoppula2007@gmail.com>
2026-05-11 15:11:25 +0530

    Revert "Merge branch 'main' of https://github.com/Varun2526/Real_time_collaborative_code-editor"
    This reverts commit b0a5a861bbe221c74a0e0030c388595a63a2d52f, reversing
    changes made to b31d23262cf20ca0265c68f1e6dec1a3ccd4f1ed.

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react';
-import TermsModal from './TermsModal';
-import { Link } from 'react-router-dom';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
-  const [isTermsOpen, setIsTermsOpen] = useState(false);
-
   return (
     <footer className="w-full border-t border-[rgba(240,240,250,0.1)] py-8 mt-auto z-10 relative bg-black">
       <div className="max-w-7xl mx-auto px-6 flex flex-col xl:flex-row items-center justify-between gap-8">
@@ -32,8 +28,6 @@ const Footer = () => {
           </div>
         </div>
       </div>
-
-      <TermsModal isOpen={isTermsOpen} onClose={() => setIsTermsOpen(false)} />
     </footer>
   );
 };
